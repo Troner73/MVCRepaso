967661c63743f0e182a187fe028ad248588fbcb2
Troner73 <163292523+Troner73@users.noreply.github.com>
2024-05-31 13:44:41 +0200

    Agregar archivos de proyecto.

---
+++ MVCRepaso/Models/SalesOrdeHeaderMetadata.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCRepaso.Models;
+
+[ModelMetadataType(typeof(SalesOrderHeaderMetadata))]
+public partial class SalesOrderHeader { }
+
+public class SalesOrderHeaderMetadata
+{
+    public int SalesOrderID { get; set; }
+
+    [Required]
+    public byte RevisionNumber { get; set; }
+    
+    [Required]
+    public DateTime OrderDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ShipDate { get; set; }
+    public byte Status { get; set; }
+    public bool OnlineOrderFlag { get; set; }
+    public string SalesOrderNumber { get; set; } = null!;
+    public string? PurchaseOrderNumber { get; set; }
+    public string? AccountNumber { get; set; }
+    public int CustomerID { get; set; }
+    public int? SalesPersonID { get; set; }
+    public int? TerritoryID { get; set; }
+    public int BillToAddressID { get; set; }
+    public int ShipToAddressID { get; set; }
+    public int ShipMethodID { get; set; }
+    public int? CreditCardID { get; set; }
+    public string? CreditCardApprovalCode { get; set; }
+    public int? CurrencyRateID { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal TaxAmt { get; set; }
+    public decimal Freight { get; set; }
+    public decimal TotalDue { get; set; }
+    public string? Comment { get; set; }
+    public Guid rowguid { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
---
+++ MVCRepaso/Models/SalesOrderDetailMetadata.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCRepaso.Models;
+
+[ModelMetadataType(typeof(SalesOrderDetailMetadata))]
+public partial class SalesOrderDetail { }
+
+public class SalesOrderDetailMetadata
+{
+    public int SalesOrderID { get; set; }
+    public int SalesOrderDetailID { get; set; }
+   
+    [Required]
+    public string? CarrierTrackingNumber { get; set; }
+    public short OrderQty { get; set; }
+    public int ProductID { get; set; }
+    public int SpecialOfferID { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal UnitPriceDiscount { get; set; }
+    public decimal LineTotal { get; set; }
+    public Guid rowguid { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
